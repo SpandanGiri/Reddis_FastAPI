0255de85258f30c77135a38766a190039db4de54
SpandanGiri <garov57775@gmail.com>
2026-08-02 18:47:28 +0530

    added reddis lock for booking seat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import redis
+from pydantic import BaseModel
+import asyncio
 
 app = FastAPI()
 
@@ -18,3 +20,27 @@ def get_item(item_id:int):
     redis_client.setex(f"item_{item_id}",3600, item_data)
 
     return {"item_id": item_id, "cached": False, "data": item_data}
+
+
+class BookingRequest(BaseModel):
+    session:str
+
+@app.post("/movies/{movie_id}/{seat_id}")
+async def book_movie(movie_id:str,seat_id:str,request_data:BookingRequest):
+
+    session = request_data.session
+
+    key = f"lock:{movie_id}:{seat_id}"
+
+    lock_acquaried = redis_client.set(key,session,nx=True,ex=120)
+
+    if not lock_acquaried:
+        return {"status": "Error"}
+
+    try:
+        await asyncio.sleep(7)
+
+        return {"status": "success", "message": f"Seat {seat_id} successfully booked!"}
+
+    finally:
+        redis_client.delete(key)
